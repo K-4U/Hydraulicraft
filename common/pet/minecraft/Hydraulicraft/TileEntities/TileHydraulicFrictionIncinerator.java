f7502ba1b5fb0babd91ef6bd40a99019b64b6ca2
Joep <digiknoops@gmail.com>
2013-12-14 11:50:49 +0100

    renamed incinerator tile entity class

---
+++ common/pet/minecraft/Hydraulicraft/TileEntities/TileHydraulicFrictionIncinerator.java
@@ -0,0 +1,19 @@
+package pet.minecraft.Hydraulicraft.TileEntities;
+
+import pet.minecraft.Hydraulicraft.baseClasses.entities.TileConsumer;
+import pet.minecraft.Hydraulicraft.lib.config.Constants;
+
+public class TileHydraulicFrictionIncinerator extends TileConsumer {
+
+	@Override
+	public void workFunction(int bar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getMaxBar() {
+		return Constants.MAX_BAR_TIER_3;
+	}
+
+}
--- common/pet/minecraft/Hydraulicraft/blocks/BlockHydraulicFrictionIncinerator.java
+++ common/pet/minecraft/Hydraulicraft/blocks/BlockHydraulicFrictionIncinerator.java
@@ -1,7 +1,9 @@
 package pet.minecraft.Hydraulicraft.blocks;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import pet.minecraft.Hydraulicraft.TileEntities.TileHydraulicFrictionIncinerator;
 import pet.minecraft.Hydraulicraft.baseClasses.MachineBlock;
 import pet.minecraft.Hydraulicraft.lib.config.Ids;
 import pet.minecraft.Hydraulicraft.lib.config.Names;
@@ -18,4 +20,13 @@ public class BlockHydraulicFrictionIncinerator extends MachineBlock {
 		// TODO Auto-generated method stub
 		return null;
 	}	
+	
+	@Override
+	public boolean onBlockActivated(World w, int x, int y, int z, EntityPlayer player, int a, float sideX, float sideY, float sideZ) {
+		if(w.isRemote) {
+			return true;
+		} else {
+			return true;
+		}
+	}
 }
