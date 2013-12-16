1054296f3995d321d972f6b96f7bbd3f3b759cfe
K-4U <koen@k-4u.nl>
2013-12-16 10:55:40 +0100

    commit from 12/16/13 10:55:40. 0 deletions, 0 adds, 2 modifications

--- common/pet/minecraft/Hydraulicraft/lib/config/ModInfo.java
+++ common/pet/minecraft/Hydraulicraft/lib/config/ModInfo.java
@@ -3,7 +3,7 @@ package pet.minecraft.Hydraulicraft.lib.config;
 public class ModInfo {
 	public static final String ID = "HydCraft";
 	public static final String NAME = "Hydraulicraft";
-	public static final String VERSION = "0.1 Alpha 1";
+	public static final String VERSION = "0.1 Alpha 1 PR3";
 	public static final String CHANNEL = ID;
 	public static final String PROXY_LOCATION = "pet.minecraft.Hydraulicraft.proxy";
 	public static final String LID = ID.toLowerCase();
