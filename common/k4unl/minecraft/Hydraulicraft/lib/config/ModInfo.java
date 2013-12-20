259301790495712f59002639d9940d48018c6b9a
K-4U <koen@k-4u.nl>
2013-12-20 17:54:26 +0100

    Changed Modinfo for new version

--- common/k4unl/minecraft/Hydraulicraft/lib/config/ModInfo.java
+++ common/k4unl/minecraft/Hydraulicraft/lib/config/ModInfo.java
@@ -3,8 +3,8 @@ package k4unl.minecraft.Hydraulicraft.lib.config;
 public class ModInfo {
 	public static final String ID = "HydCraft";
 	public static final String NAME = "Hydraulicraft";
-	public static final String VERSION = "0.1 Alpha 1 PR3";
+	public static final String VERSION = "0.0.1 Alpha 3";
 	public static final String CHANNEL = ID;
-	public static final String PROXY_LOCATION = "pet.minecraft.Hydraulicraft.proxy";
+	public static final String PROXY_LOCATION = "k4unl.minecraft.Hydraulicraft.proxy";
 	public static final String LID = ID.toLowerCase();
 }
