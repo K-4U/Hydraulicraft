052f6cdc7c4a48122c655385687446e9c1d24ea2
MineMaarten <loopycoaster_8@hotmail.com>
2013-12-30 10:47:06 +0100

    Added progress bar for the crusher.

--- java/k4unl/minecraft/Hydraulicraft/client/GUI/GuiCrusher.java
+++ java/k4unl/minecraft/Hydraulicraft/client/GUI/GuiCrusher.java
@@ -1,37 +1,41 @@
 package k4unl.minecraft.Hydraulicraft.client.GUI;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import k4unl.minecraft.Hydraulicraft.TileEntities.TileHydraulicCrusher;
 import k4unl.minecraft.Hydraulicraft.baseClasses.MachineGUI;
 import k4unl.minecraft.Hydraulicraft.containers.ContainerCrusher;
-import k4unl.minecraft.Hydraulicraft.lib.config.Constants;
 import k4unl.minecraft.Hydraulicraft.lib.config.ModInfo;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.StatCollector;
 
 import org.lwjgl.opengl.GL11;
 
-public class GuiCrusher extends MachineGUI {
-	private static final ResourceLocation resLoc = new ResourceLocation(ModInfo.LID,"textures/gui/crusher.png");
-	TileHydraulicCrusher crusher;
-	
-	
-	public GuiCrusher(InventoryPlayer invPlayer, TileHydraulicCrusher _crusher) {
-		super(_crusher, new ContainerCrusher(invPlayer, _crusher), resLoc);
-		crusher = _crusher;
-	}
+public class GuiCrusher extends MachineGUI{
+    private static final ResourceLocation resLoc = new ResourceLocation(ModInfo.LID, "textures/gui/crusher.png");
+    TileHydraulicCrusher crusher;
 
-	@Override
-	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY){
-		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
-		fontRenderer.drawString(Names.blockHydraulicCrusher.localized, 8, 6, 0xFFFFFF);
-		
-		drawFluidAndPressure();
-		checkTooltips(mouseX, mouseY);
-		
-	}
+    public GuiCrusher(InventoryPlayer invPlayer, TileHydraulicCrusher _crusher){
+        super(_crusher, new ContainerCrusher(invPlayer, _crusher), resLoc);
+        crusher = _crusher;
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float f, int i, int j){
+        super.drawGuiContainerBackgroundLayer(f, i, j);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        int xStart = (width - xSize) / 2;
+        int yStart = (height - ySize) / 2;
+        int scaledProgress = crusher.getScaledCrushTime();
+        drawTexturedModalRect(xStart + 85, yStart + 32, 207, 0, scaledProgress, 17);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY){
+        super.drawGuiContainerForegroundLayer(mouseX, mouseY);
+        fontRenderer.drawString(Names.blockHydraulicCrusher.localized, 8, 6, 0xFFFFFF);
+        drawFluidAndPressure();
+        checkTooltips(mouseX, mouseY);
+    }
 }
