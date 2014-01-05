d4404364e8bb6f9e342c8fbb3872d0c03f60562d
K-4U <koen@k-4u.nl>
2014-01-05 15:17:51 +0100

    Added recipe for the pneumatic compressor

--- java/k4unl/minecraft/Hydraulicraft/thirdParty/pneumaticraft/Pneumaticraft.java
+++ java/k4unl/minecraft/Hydraulicraft/thirdParty/pneumaticraft/Pneumaticraft.java
@@ -1,9 +1,13 @@
 package k4unl.minecraft.Hydraulicraft.thirdParty.pneumaticraft;
 
+import pneumaticCraft.api.block.BlockSupplier;
+import k4unl.minecraft.Hydraulicraft.blocks.Blocks;
+import k4unl.minecraft.Hydraulicraft.items.Items;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
 import k4unl.minecraft.Hydraulicraft.thirdParty.pneumaticraft.blocks.BlockHydraulicPneumaticCompressor;
 import k4unl.minecraft.Hydraulicraft.thirdParty.pneumaticraft.tileEntities.TileHydraulicPneumaticCompressor;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
@@ -14,6 +18,7 @@ public class Pneumaticraft {
 	public static void init(){
 		
 		initBlocks();
+		initRecipes();
 	}
 	
 	public static void initBlocks(){
@@ -22,5 +27,18 @@ public class Pneumaticraft {
 		LanguageRegistry.addName(hydraulicPneumaticCompressor, Names.blockHydraulicPneumaticCompressor.localized);
 		
 		GameRegistry.registerTileEntity(TileHydraulicPneumaticCompressor.class, "tileHydraulicPneumaticCompressor");
+	}
+	
+	public static void initRecipes(){
+		GameRegistry.addRecipe(new ItemStack(hydraulicPneumaticCompressor, 1),
+				new Object [] {
+					"WWW",
+					"KCT",
+					"WWW",
+					'K', Items.gasket,
+					'T', new ItemStack(BlockSupplier.getBlock("pressureTube"), 1, 0),
+					'W', Blocks.hydraulicPressureWall,
+					'C', BlockSupplier.getBlock("airCompressor")
+				});
 	}
 }
