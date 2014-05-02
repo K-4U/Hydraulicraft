24ed4dab0982866e5339d3cd62f40f2dd0d49a16
K-4U <koen@k-4u.nl>
2014-05-03 01:35:51 +0200

    Added newer version of Waila API

--- java/mcp/mobius/waila/api/IWailaFMPAccessor.java
+++ java/mcp/mobius/waila/api/IWailaFMPAccessor.java
@@ -1,6 +1,7 @@
 package mcp.mobius.waila.api;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MovingObjectPosition;
