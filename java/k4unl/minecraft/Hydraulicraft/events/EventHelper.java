af5cddfcafe86fd1511e468a4ef558bd6faf5ed9
K-4U <koen@k-4u.nl>
2014-05-07 19:29:04 +0200

    Fixed update checker spamming

--- java/k4unl/minecraft/Hydraulicraft/events/EventHelper.java
+++ java/k4unl/minecraft/Hydraulicraft/events/EventHelper.java
@@ -91,6 +91,7 @@ public class EventHelper {
 							break;
 						}
 					}
+					hasShownUpdateInfo = true;
 				}else{
 					Functions.showMessageInChat(((EntityPlayer) event.entity), "Hydraulicraft up to date (" + ModInfo.VERSION + "-" + ModInfo.buildNumber + ")");
 				}
