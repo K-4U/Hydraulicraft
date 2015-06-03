be4c12b2e3d9621c94243e158779f98ae2515ae7
Koen Beckers <koen@k-4u.nl>
2015-06-03 15:37:52 +0200

    Fluid transport is done.

--- java/k4unl/minecraft/Hydraulicraft/multipart/PartFluidInterface.java
+++ java/k4unl/minecraft/Hydraulicraft/multipart/PartFluidInterface.java
@@ -4,71 +4,98 @@ import codechicken.lib.data.MCDataInput;
 import codechicken.lib.data.MCDataOutput;
 import codechicken.lib.raytracer.IndexedCuboid6;
 import codechicken.lib.vec.Cuboid6;
+import codechicken.lib.vec.Vector3;
 import codechicken.microblock.ISidedHollowConnect;
 import codechicken.multipart.*;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import k4unl.minecraft.Hydraulicraft.client.renderers.transportation.RendererPartFluidPipe;
+import k4unl.minecraft.Hydraulicraft.client.renderers.transportation.RendererPartFluidInterface;
 import k4unl.minecraft.Hydraulicraft.lib.Functions;
+import k4unl.minecraft.Hydraulicraft.lib.config.Constants;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
+import k4unl.minecraft.k4lib.client.RenderHelper;
+import k4unl.minecraft.k4lib.lib.Location;
+import k4unl.minecraft.k4lib.lib.Vector3fMax;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.*;
+import org.lwjgl.opengl.GL11;
 
 import java.util.LinkedList;
 
 
-public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSlottedPart, JNormalOcclusion, ISidedHollowConnect {
-    private static float pixel = 1.0F / 16F;
-    private static int expandBounds = -1;
-    private ForgeDirection side = ForgeDirection.DOWN;
+public class PartFluidInterface extends TMultiPart implements IFluidHandler, TFacePart, TSlottedPart, JNormalOcclusion, ISidedHollowConnect {
+    private static float          pixel        = 1.0F / 16F;
+    private static int            expandBounds = -1;
+    private        ForgeDirection side         = ForgeDirection.DOWN;
 
     public static Cuboid6[] boundingBoxes = new Cuboid6[6];
 
     private FluidTank tank = new FluidTank(1000);
 
     @SideOnly(Side.CLIENT)
-    private static RendererPartFluidPipe renderer;
+    private static RendererPartFluidInterface renderer;
 
     @SideOnly(Side.CLIENT)
     private static IIcon breakIcon;
 
     static {
         float center = 0.5F;
-        double w = pixel*4;
+        double w = pixel * 4;
         //boundingBoxes[6] = new Cuboid6(center - w, center - w, center - w, center + w, center + w, center + w);
 
+        float thickness = 2 * RenderHelper.pixel;
+        float width = 8 * RenderHelper.pixel;
+        float min = 0.5F - (width/2);
+        float max = 0.5F + (width/2);
+        float tMax = 1.0F - thickness;
+        float tMin = 0.0F + thickness;
+        Vector3fMax vector;
         int i = 0;
-        for(ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS){
-            double xMin1 = (dir.offsetX < 0 ? 0.0 : (dir.offsetX == 0 ? center - w : center + w));
-            double xMax1 = (dir.offsetX > 0 ? 1.0 : (dir.offsetX == 0 ? center + w : center - w));
+        for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
 
-            double yMin1 = (dir.offsetY < 0 ? 0.0 : (dir.offsetY == 0 ? center - w : center + w));
-            double yMax1 = (dir.offsetY > 0 ? 1.0 : (dir.offsetY == 0 ? center + w : center - w));
+            if(dir.equals(ForgeDirection.UP)){
+                vector = new Vector3fMax(min, tMax, min, max, 1.0F, max);
+            }else if(dir.equals(ForgeDirection.DOWN)){
+                vector = new Vector3fMax(min, 0.0F, min, max, tMin, max);
+            }else if(dir.equals(ForgeDirection.NORTH)){
+                vector = new Vector3fMax(min, min, 0.0F, max, max, tMin);
+            }else if(dir.equals(ForgeDirection.SOUTH)){
+                vector = new Vector3fMax(min, min, tMax, max, max, 1.0F);
+            }else if(dir.equals(ForgeDirection.WEST)){
+                vector = new Vector3fMax(0.0F, min, min, tMin, max, max);
+            }else if(dir.equals(ForgeDirection.EAST)){
+                vector = new Vector3fMax(tMax, min, min, 1.0F, max, max);
+            }else{
+                vector = new Vector3fMax(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+            }
 
-            double zMin1 = (dir.offsetZ < 0 ? 0.0 : (dir.offsetZ == 0 ? center - w : center + w));
-            double zMax1 = (dir.offsetZ > 0 ? 1.0 : (dir.offsetZ == 0 ? center + w : center - w));
-
-            boundingBoxes[i] = new Cuboid6(xMin1, yMin1, zMin1, xMax1, yMax1, zMax1);
+            boundingBoxes[i] = new Cuboid6(vector.getXMin(), vector.getYMin(), vector.getZMin(), vector.getXMax(), vector.getYMax(), vector.getZMax());
             i++;
         }
     }
 
+    private boolean isRedstonePowered;
+
     public void preparePlacement(ForgeDirection side_) {
+
         side = side_;
     }
 
     @Override
     public int fill(ForgeDirection from, FluidStack resource, boolean doFill) {
-        if(!from.equals(side))
+
+        if (!from.equals(side))
             return 0;
         return tank.fill(resource, doFill);
     }
 
     @Override
     public FluidStack drain(ForgeDirection from, FluidStack resource, boolean doDrain) {
-        if(!from.equals(side))
+
+        if (!from.equals(side))
             return null;
 
         return null;
@@ -76,7 +103,8 @@ public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSl
 
     @Override
     public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain) {
-        if(!from.equals(side))
+
+        if (!from.equals(side))
             return null;
 
         return tank.drain(maxDrain, doDrain);
@@ -84,41 +112,49 @@ public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSl
 
     @Override
     public boolean canFill(ForgeDirection from, Fluid fluid) {
+
         return from.equals(side);
     }
 
     @Override
     public boolean canDrain(ForgeDirection from, Fluid fluid) {
+
         return from.equals(side);
     }
 
     @Override
     public FluidTankInfo[] getTankInfo(ForgeDirection from) {
-        return new FluidTankInfo[] {new FluidTankInfo(tank)};
+
+        return new FluidTankInfo[] { new FluidTankInfo(tank) };
     }
 
     @Override
     public String getType() {
+
         return "tile." + Names.partFluidInterface.unlocalized;
     }
 
     @Override
     public int getSlotMask() {
+
         return PartMap.face(side.ordinal()).mask;
     }
 
     @Override
     public int getHollowSize(int i) {
+
         return 10;
     }
 
     @Override
-    public Iterable<Cuboid6> getOcclusionBoxes(){
+    public Iterable<Cuboid6> getOcclusionBoxes() {
+
         return getCollisionBoxes();
     }
 
     @Override
-    public Iterable<Cuboid6> getCollisionBoxes(){
+    public Iterable<Cuboid6> getCollisionBoxes() {
+
         LinkedList<Cuboid6> list = new LinkedList<Cuboid6>();
         list.add(boundingBoxes[Functions.getIntDirFromDirection(side)]);
         return list;
@@ -126,6 +162,7 @@ public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSl
 
     @Override
     public Iterable<IndexedCuboid6> getSubParts() {
+
         Iterable<Cuboid6> boxList = getCollisionBoxes();
         LinkedList<IndexedCuboid6> partList = new LinkedList<IndexedCuboid6>();
         for (Cuboid6 c : boxList)
@@ -134,12 +171,14 @@ public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSl
     }
 
     @Override
-    public boolean occlusionTest(TMultiPart npart){
+    public boolean occlusionTest(TMultiPart npart) {
+
         return NormalOcclusionTest.apply(this, npart);
     }
 
     @Override
-    public void load(NBTTagCompound tagCompound){
+    public void load(NBTTagCompound tagCompound) {
+
         super.load(tagCompound);
         //connectedSides = new HashMap<ForgeDirection, TileEntity>();
         //getHandler().updateNetworkOnNextTick(oldPressure);
@@ -196,16 +235,17 @@ public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSl
     public void update(){
         super.update();
 
+
         if(!world().isRemote && tank != null && tank.getFluid() != null) {
             //Find a fluid pipe
             if (Multipart.hasPartFluidPipe(tile())) {
                 PartFluidPipe pipe = Multipart.getFluidPipe(tile());
-                if(pipe.getFluidStored() == null){
+                if (pipe.getFluidStored() == null) {
                     pipe.setFluidStored(tank.getFluid().getFluid());
                 }
-                if (pipe.getFluidAmountStored() < 100) {
-                    double fluidAdded = 100 - pipe.getFluidAmountStored();
-                    if (tank.drain((int) fluidAdded, false).amount >= (int)fluidAdded) {
+                if (pipe.getFluidAmountStored() < Constants.MAX_FLUID_TRANSFER_T * 10) {
+                    double fluidAdded = (Constants.MAX_FLUID_TRANSFER_T * 10) - pipe.getFluidAmountStored();
+                    if (tank.drain((int) fluidAdded, false).amount >= (int) fluidAdded) {
                         pipe.addFluid(fluidAdded);
                         tank.drain((int) fluidAdded, true);
                     }
@@ -213,6 +253,89 @@ public class PartFluidInterface extends TMultiPart implements IFluidHandler, TSl
                 }
             }
         }
+        if(!world().isRemote){
+            if (getIsRedstonePowered()){
+                //Find a tank on the side we're on.
+                Location tankLocation = new Location(x(), y(), z(), side);
+                TileEntity te = tankLocation.getTE(world());
+                if(te instanceof IFluidHandler){
+                    if(tank.getFluid() == null){
+                        drainFluid((IFluidHandler)te);
+                    } else if(tank.getFluid() != null){
+                        if(tank.getFluidAmount() == 0){
+                            drainFluid((IFluidHandler) te);
+                        }else{
+                            FluidTankInfo tankInfo = ((IFluidHandler)te).getTankInfo(side.getOpposite())[0];
+                            if(tankInfo != null && tankInfo.fluid != null) {
+                                if (tank.getFluid().getFluid() == tankInfo.fluid.getFluid()) {
+                                    drainFluid((IFluidHandler) te);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
+    private void drainFluid(IFluidHandler fluidHandler){
+        FluidStack drained = fluidHandler.drain(side.getOpposite(), Constants.MAX_FLUID_TRANSFER_T, false);
+        if(drained != null && drained.amount > 0) {
+            int filled = tank.fill(drained, false);
+            if(filled > 0){
+                //Do the actual filling.
+                drained = fluidHandler.drain(side.getOpposite(), filled, true);
+                tank.fill(drained, true);
+            }
+        }
+    }
+
+    public void checkRedstonePower() {
+        boolean isIndirectlyPowered = world().isBlockIndirectlyGettingPowered(x(), y(), z());
+        if(isIndirectlyPowered && !getIsRedstonePowered()){
+            setRedstonePowered(true);
+        }else if(getIsRedstonePowered() && !isIndirectlyPowered){
+            setRedstonePowered(false);
+        }
+    }
+
+    public boolean getIsRedstonePowered() {
+        return isRedstonePowered;
+    }
+
+    public void setRedstonePowered(boolean isRedstonePowered) {
+        this.isRedstonePowered = isRedstonePowered;
+    }
+
+
+    public void onNeighborChanged(){
+        if(!world().isRemote){
+            checkRedstonePower();
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void renderDynamic(Vector3 pos, float frame, int pass){
+        if (pass == 0){
+            if(renderer == null){
+                renderer = new RendererPartFluidInterface();
+            }
+            GL11.glDisable(GL11.GL_LIGHTING);
+            renderer.doRender(pos.x, pos.y, pos.z, frame, side);
+            GL11.glEnable(GL11.GL_LIGHTING);
+        }
+    }
+
+    @Override
+    public boolean solid(int i) {
+
+        return true;
+    }
+
+    @Override
+    public int redstoneConductionMap() {
+
+        return 0;
+    }
 }
