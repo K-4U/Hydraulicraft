5ea320d52635c4a2b543e108b7ab8779a3c9b76c
Koen Beckers <koen@k-4u.nl>
2015-06-16 14:52:27 +0200

    Apparently forgot to rename a container

--- java/k4unl/minecraft/Hydraulicraft/containers/ContainerFilter.java
+++ java/k4unl/minecraft/Hydraulicraft/containers/ContainerFilter.java
@@ -1,14 +1,14 @@
 package k4unl.minecraft.Hydraulicraft.containers;
 
 import k4unl.minecraft.Hydraulicraft.slots.SlotMachineInput;
-import k4unl.minecraft.Hydraulicraft.tileEntities.consumers.TileHydraulicMixer;
+import k4unl.minecraft.Hydraulicraft.tileEntities.consumers.TileHydraulicFilter;
 import net.minecraft.entity.player.InventoryPlayer;
 
-public class ContainerMixer extends ContainerBase {
+public class ContainerFilter extends ContainerBase {
 
-    public ContainerMixer(InventoryPlayer invPlayer, TileHydraulicMixer mixer) {
-        super(mixer);
-        addSlotToContainer(new SlotMachineInput(mixer, mixer, 0, 64, 17));
+    public ContainerFilter(InventoryPlayer invPlayer, TileHydraulicFilter filter) {
+        super(filter);
+        addSlotToContainer(new SlotMachineInput(filter, filter, 0, 64, 17));
 
         bindPlayerInventory(invPlayer);
     }
