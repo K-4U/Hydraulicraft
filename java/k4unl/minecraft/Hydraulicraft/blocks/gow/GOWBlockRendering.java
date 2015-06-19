31149e3f6e557b947dd59132e9ba62f7e3d78a40
Koen Beckers <koen@k-4u.nl>
2015-06-19 18:06:00 +0200

    Fixes #126

--- java/k4unl/minecraft/Hydraulicraft/blocks/gow/GOWBlockRendering.java
+++ java/k4unl/minecraft/Hydraulicraft/blocks/gow/GOWBlockRendering.java
@@ -1,10 +1,14 @@
 package k4unl.minecraft.Hydraulicraft.blocks.gow;
 
 
+import k4unl.minecraft.Hydraulicraft.lib.config.ModInfo;
+import k4unl.minecraft.Hydraulicraft.lib.config.Names;
+
 public abstract class GOWBlockRendering extends GOWBlockBase {
 
 	protected GOWBlockRendering(String name) {
 		super(name);
+		setBlockTextureName(ModInfo.LID + ":" + Names.blockHydraulicPressureWall.unlocalized + "/" + Names.blockHydraulicPressureWall.unlocalized + "_1");
 	}
 
 	@Override
