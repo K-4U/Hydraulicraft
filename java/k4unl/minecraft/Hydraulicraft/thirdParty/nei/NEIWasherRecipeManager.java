b4084be6db84d865bff4653ebd189e114ba6ca1a
OndraSter <ondraster@gmail.com>
2015-06-21 21:15:44 +0200

    Added NEI plugin for washer

--- java/k4unl/minecraft/Hydraulicraft/thirdParty/nei/NEIWasherRecipeManager.java
+++ java/k4unl/minecraft/Hydraulicraft/thirdParty/nei/NEIWasherRecipeManager.java
@@ -1,132 +1,109 @@
 package k4unl.minecraft.Hydraulicraft.thirdParty.nei;
 
-import java.awt.Rectangle;
-import java.util.LinkedList;
-
-import k4unl.minecraft.Hydraulicraft.client.GUI.GuiCrusher;
+import codechicken.nei.PositionedStack;
+import k4unl.minecraft.Hydraulicraft.api.recipes.IFluidRecipe;
+import k4unl.minecraft.Hydraulicraft.client.GUI.GuiWasher;
 import k4unl.minecraft.Hydraulicraft.client.GUI.IconRenderer;
 import k4unl.minecraft.Hydraulicraft.lib.Localization;
-import k4unl.minecraft.Hydraulicraft.lib.WashingRecipes;
 import k4unl.minecraft.Hydraulicraft.lib.config.ModInfo;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
+import k4unl.minecraft.Hydraulicraft.lib.recipes.HydraulicRecipes;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
-import codechicken.nei.NEIClientUtils;
-import codechicken.nei.PositionedStack;
-import codechicken.nei.recipe.ShapedRecipeHandler;
+
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
 
 
-public class NEIWasherRecipeManager extends ShapedRecipeHandler{
-    private ShapedRecipeHandler.CachedShapedRecipe getShape(WashingRecipes.WashingRecipe recipe) {
-        ShapedRecipeHandler.CachedShapedRecipe shape = new
-                ShapedRecipeHandler.CachedShapedRecipe(0, 0, null, recipe.output);
+public class NEIWasherRecipeManager extends NEIHydraulicRecipePlugin {
 
-
-        PositionedStack stack = new PositionedStack(recipe.input, 51, 5);
-        stack.setMaxSize(2);
-        shape.ingredients.add(stack);
-        shape.result.relx = 101;
-        shape.result.rely = 45;
-        return shape;
+    @Override
+    public Class<? extends GuiContainer> getGuiClass() {
+        return GuiWasher.class;
     }
 
     @Override
-    public void loadCraftingRecipes(ItemStack result) {
-        for(WashingRecipes.WashingRecipe recipe: WashingRecipes.washingRecipes) {
-            if(NEIClientUtils.areStacksSameTypeCrafting(recipe.output, result)) {
-                this.arecipes.add(getShape(recipe));
-            }
-        }
-    }
-
-    @Override
-    public Class<? extends GuiContainer> getGuiClass(){
-        return GuiCrusher.class;
-    }
-
-    @Override
-    public String getRecipeName(){
+    public String getRecipeName() {
         return Localization.getLocalizedName(Names.blockHydraulicWasher.unlocalized);
     }
 
     @Override
-    public String getGuiTexture(){
+    public String getGuiTexture() {
         return ModInfo.LID + ":textures/gui/washer.png";
     }
 
 
     @Override
-    public boolean hasOverlay(GuiContainer gui, Container container, int recipe)
-    {
+    public boolean hasOverlay(GuiContainer gui, Container container, int recipe) {
         return false;
     }
 
     @Override
-    public void loadUsageRecipes(ItemStack ingredient) {
-        for(WashingRecipes.WashingRecipe recipe: WashingRecipes.washingRecipes) {
-            if(NEIClientUtils.areStacksSameTypeCrafting(recipe.input,
-                    ingredient)) {
-                this.arecipes.add(getShape(recipe));
-                break;
-            }
-        }
+    public void loadTransferRects() {
+        transferRects = new LinkedList<RecipeTransferRect>();
+        transferRects.add(new RecipeTransferRect(new Rectangle(27, 30, 20, 35), "washing"));
     }
-
-
 
     @Override
     public void loadCraftingRecipes(String outputId, Object... results) {
-        if(outputId.equals("washing")){
-            for(WashingRecipes.WashingRecipe recipe: WashingRecipes.washingRecipes) {
+        if (outputId.equals("washing")) {
+            for (IFluidRecipe recipe : HydraulicRecipes.getWasherRecipes()) {
                 this.arecipes.add(getShape(recipe));
             }
-        }else{
-        	super.loadCraftingRecipes(outputId, results);
+        } else
+            super.loadCraftingRecipes(outputId, results);
+    }
+
+    @Override
+    public void drawExtras(int recipeIndex) {
+        int ticks = 48;
+        float percentage = cycleticks % ticks / (float) ticks;
+
+        CachedRecipe recipe = arecipes.get(recipeIndex);
+
+
+        ItemStack washingItem = recipe.getIngredients().get(0).item;
+        ItemStack targetItem = recipe.getResult().item;
+        int startY = 5;
+        int startX = 51;
+        int targetX = 101;
+        int targetY = 0;
+        int travelPath = 0;
+        int xPos = startX;
+        int yPos = startY;
+
+        if (percentage < 0.25F) {
+            targetX = 75;
+            travelPath = (targetX - startX) * 4;
+            xPos = startX + (int) (travelPath * percentage);
+        } else if (percentage < 0.75F) {
+            xPos = 75;
+            targetY = 45;
+            travelPath = targetY - startY;
+            yPos = startY + (int) (travelPath * ((percentage - 0.25F) * 2));
+        } else {
+            yPos = 45;
+            targetX = 101;
+            startX = 75;
+            travelPath = targetX - startX;
+            xPos = startX + (int) (travelPath * (percentage - 0.75F) * 4);
         }
+
+        IconRenderer.drawMergedIcon(xPos, yPos, 0.0F, washingItem, targetItem, percentage, true);
     }
-    
+
     @Override
-    public void loadTransferRects(){
-    	transferRects = new LinkedList<RecipeTransferRect>();
-        transferRects.add(new RecipeTransferRect(new Rectangle(74, 28, 35, 20), "washing"));
+    public CachedRecipe getShape(IFluidRecipe recipe) {
+        NEIHydraulicRecipe nei = new NEIHydraulicRecipe();
+        nei.addInput(new PositionedStack(recipe.getInputItems()[0], 52, 6));
+        nei.addOutput(new PositionedStack(recipe.getRecipeOutput(), 101, 46));
+        return nei;
     }
-    
+
     @Override
-    public void drawExtras(int recipeIndex){
-    	int ticks = 48;
-    	float percentage = cycleticks % ticks / (float)ticks;
-    	
-    	CachedRecipe recipe = arecipes.get(recipeIndex); 
-    	
-    	
-    	ItemStack washingItem = recipe.getIngredients().get(0).item;
-		ItemStack targetItem = recipe.getResult().item;	
-		int startY = 5;
-		int startX = 51;
-		int targetX = 101;
-		int targetY = 0;
-		int travelPath = 0;
-		int xPos = startX;
-		int yPos = startY;
-		
-		if(percentage < 0.25F){
-			targetX = 75;
-			travelPath = (targetX - startX) * 4;
-			xPos = startX + (int) (travelPath * percentage);
-		}else if(percentage < 0.75F){
-			xPos = 75;
-			targetY = 45;
-			travelPath = targetY - startY;
-			yPos = startY + (int) (travelPath * ((percentage-0.25F)*2));
-		}else{
-			yPos = 45;
-			targetX = 101;
-			startX = 75;
-			travelPath = targetX - startX;
-			xPos = startX + (int) (travelPath * (percentage-0.75F)*4);
-		}
-		
-		IconRenderer.drawMergedIcon(xPos, yPos, 0.0F, washingItem, targetItem, percentage, true);
+    public List<IFluidRecipe> getRecipeCollection() {
+        return HydraulicRecipes.getWasherRecipes();
     }
 }
