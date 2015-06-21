17dcccae89ad74b5deb8a16437f9f1f126033d22
OndraSter <ondraster@gmail.com>
2015-06-22 01:21:31 +0200

    Prevent unnecessary creation of NBT killing shift-clicking in NEI

--- java/k4unl/minecraft/Hydraulicraft/items/HydraulicItemBase.java
+++ java/k4unl/minecraft/Hydraulicraft/items/HydraulicItemBase.java
@@ -1,7 +1,5 @@
 package k4unl.minecraft.Hydraulicraft.items;
 
-import java.util.List;
-
 import k4unl.minecraft.Hydraulicraft.lib.CustomTabs;
 import k4unl.minecraft.Hydraulicraft.lib.config.ModInfo;
 import k4unl.minecraft.Hydraulicraft.lib.helperClasses.Name;
@@ -10,54 +8,56 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
+import java.util.List;
+
 public class HydraulicItemBase extends Item {
-	private Name mName;
-	private boolean hasEffect = false;
-	private String defaultInfo = "";
-	
-	
-	public HydraulicItemBase(Name itemName) {
-		super();
-		
-		mName = itemName;
-		
-		setMaxStackSize(64);
-		setUnlocalizedName(itemName.unlocalized);
-		setTextureName(ModInfo.LID + ":" + itemName.unlocalized);
-		
-		setCreativeTab(CustomTabs.tabHydraulicraft);
-	}
-	
-	/*!
-	 * @author Koen Beckers
-	 * @date 13-12-2013
-	 * Sets whether the "enchanted" effect is active on this item.
-	 */
-    public void setEffect(boolean _hasEffect){
+    private Name mName;
+    private boolean hasEffect   = false;
+    private String  defaultInfo = "";
+
+
+    public HydraulicItemBase(Name itemName) {
+        super();
+
+        mName = itemName;
+
+        setMaxStackSize(64);
+        setUnlocalizedName(itemName.unlocalized);
+        setTextureName(ModInfo.LID + ":" + itemName.unlocalized);
+
+        setCreativeTab(CustomTabs.tabHydraulicraft);
+    }
+
+    /*!
+     * @author Koen Beckers
+     * @date 13-12-2013
+     * Sets whether the "enchanted" effect is active on this item.
+     */
+    public void setEffect(boolean _hasEffect) {
         hasEffect = _hasEffect;
     }
 
-    public void setEffect(ItemStack itemStack, boolean _hasEffect){
+    public void setEffect(ItemStack itemStack, boolean _hasEffect) {
         NBTTagCompound stackCompound = itemStack.getTagCompound();
         stackCompound.setBoolean("hasEffect", _hasEffect);
         itemStack.setTagCompound(stackCompound);
     }
 
     @Override
-    public boolean hasEffect(ItemStack itemStack, int pass){
-        if(itemStack.getTagCompound() == null){
-            itemStack.setTagCompound(new NBTTagCompound());
+    public boolean hasEffect(ItemStack itemStack, int pass) {
+        if (itemStack.getTagCompound() == null) {
+            return hasEffect;
         }
 
         return hasEffect || itemStack.getTagCompound().getBoolean("hasEffect");
     }
 
-    public void setDefaultInfo(String info){
+    public void setDefaultInfo(String info) {
         defaultInfo = info;
     }
 
-    public void setDefaultInfo(ItemStack itemStack, String info){
-        if(itemStack.getTagCompound() == null){
+    public void setDefaultInfo(ItemStack itemStack, String info) {
+        if (itemStack.getTagCompound() == null) {
             itemStack.setTagCompound(new NBTTagCompound());
         }
         NBTTagCompound stackCompound = itemStack.getTagCompound();
@@ -66,12 +66,12 @@ public class HydraulicItemBase extends Item {
     }
 
     @Override
-    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4){
-        if(defaultInfo != ""){
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4) {
+        if (defaultInfo != "") {
             list.add(defaultInfo);
         }
-        if(itemStack.getTagCompound() != null){
-            if(itemStack.getTagCompound().getString("defaultInfo") != ""){
+        if (itemStack.getTagCompound() != null) {
+            if (itemStack.getTagCompound().getString("defaultInfo") != "") {
                 list.add(itemStack.getTagCompound().getString("defaultInfo"));
             }
         }
