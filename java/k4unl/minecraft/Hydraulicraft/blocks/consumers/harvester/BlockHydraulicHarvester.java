2c59f73d2982d01cb67a759ded3b6d6b3873d4e3
Koen Beckers <koen@k-4u.nl>
2015-06-22 21:06:51 +0200

    Oops

--- java/k4unl/minecraft/Hydraulicraft/blocks/consumers/harvester/BlockHydraulicHarvester.java
+++ java/k4unl/minecraft/Hydraulicraft/blocks/consumers/harvester/BlockHydraulicHarvester.java
@@ -1,13 +1,11 @@
 package k4unl.minecraft.Hydraulicraft.blocks.consumers.harvester;
 
-import k4unl.minecraft.Hydraulicraft.Hydraulicraft;
 import k4unl.minecraft.Hydraulicraft.api.ITieredBlock;
 import k4unl.minecraft.Hydraulicraft.api.PressureTier;
 import k4unl.minecraft.Hydraulicraft.blocks.HydraulicBlockContainerBase;
 import k4unl.minecraft.Hydraulicraft.lib.config.GuiIDs;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
 import k4unl.minecraft.Hydraulicraft.tileEntities.harvester.TileHydraulicHarvester;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -30,27 +28,8 @@ public class BlockHydraulicHarvester extends HydraulicBlockContainerBase impleme
     @Override
     public GuiIDs getGUIID() {
 
-        return GuiIDs.INVALID;
+        return GuiIDs.HARVESTER;
     }
-
-    @Override
-    public boolean onBlockActivated(World world, int x, int y, int z,
-      EntityPlayer player, int par6, float par7, float par8, float par9) {
-
-        TileEntity entity = world.getTileEntity(x, y, z);
-        if (entity == null || !(entity instanceof TileHydraulicHarvester)) {
-            return false;
-
-        }
-
-        TileHydraulicHarvester harvester = (TileHydraulicHarvester) entity;
-        if (harvester.getIsMultiblock()) {
-            player.openGui(Hydraulicraft.instance, GuiIDs.HARVESTER.ordinal(), world, x, y, z);
-            return true;
-		}
-
-		return false;
-	}
 
 
     @Override
