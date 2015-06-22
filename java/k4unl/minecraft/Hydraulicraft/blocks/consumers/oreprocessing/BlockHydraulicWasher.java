ca938dd316731a91587993679d8098bebc2e5560
Koen Beckers <koen@k-4u.nl>
2015-06-22 21:11:40 +0200

    Now i just feel stupid..

--- java/k4unl/minecraft/Hydraulicraft/blocks/consumers/oreprocessing/BlockHydraulicWasher.java
+++ java/k4unl/minecraft/Hydraulicraft/blocks/consumers/oreprocessing/BlockHydraulicWasher.java
@@ -1,6 +1,5 @@
 package k4unl.minecraft.Hydraulicraft.blocks.consumers.oreprocessing;
 
-import k4unl.minecraft.Hydraulicraft.Hydraulicraft;
 import k4unl.minecraft.Hydraulicraft.api.IMultiTieredBlock;
 import k4unl.minecraft.Hydraulicraft.api.PressureTier;
 import k4unl.minecraft.Hydraulicraft.blocks.HydraulicBlockContainerBase;
@@ -10,7 +9,6 @@ import k4unl.minecraft.Hydraulicraft.lib.Localization;
 import k4unl.minecraft.Hydraulicraft.lib.config.GuiIDs;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
 import k4unl.minecraft.Hydraulicraft.tileEntities.consumers.TileHydraulicWasher;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -31,25 +29,6 @@ public class BlockHydraulicWasher extends HydraulicBlockContainerBase implements
 	public GuiIDs getGUIID() {
 
 		return GuiIDs.WASHER;
-	}
-
-	@Override
-	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9){
-		if(player.isSneaking())
-			return false;
-		
-		TileEntity entity = world.getTileEntity(x, y, z);
-		if(entity == null || !(entity instanceof TileHydraulicWasher)){
-			return false;
-			
-		}
-		TileHydraulicWasher washer = (TileHydraulicWasher) entity;
-		if(washer.getIsValidMultiblock()){
-			player.openGui(Hydraulicraft.instance, getGUIID().ordinal(), world, x, y, z);
-			return true;
-		}
-		
-		return false;
 	}
 
     @Override
