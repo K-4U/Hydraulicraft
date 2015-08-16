13f1eb1ff55427007d7049b1be0d2d507ed18d0d
OndraSter <ondraster@gmail.com>
2015-08-16 12:11:35 +0200

    Change wrench to use the prototype instead...

--- java/k4unl/minecraft/Hydraulicraft/items/ItemHydraulicWrench.java
+++ java/k4unl/minecraft/Hydraulicraft/items/ItemHydraulicWrench.java
@@ -6,16 +6,15 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import k4unl.minecraft.Hydraulicraft.api.IPressurizableItem;
 import k4unl.minecraft.Hydraulicraft.fluids.Fluids;
+import k4unl.minecraft.Hydraulicraft.lib.PressurizableItem;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
-import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
 import java.util.List;
@@ -27,41 +26,43 @@ public class ItemHydraulicWrench extends HydraulicItemBase implements IPressuriz
     public static final int   FLUID_CAPACITY          = 20;
     public static final float CHANCE_TO_RELEASE_WATER = 0.1f;
 
+    private PressurizableItem pressurizableItem;
+
     public ItemHydraulicWrench() {
         super(Names.itemHydraulicWrench);
         setNoRepair();
         maxStackSize = 1;
-
+        pressurizableItem = new PressurizableItem(MAX_PRESSURE, FLUID_CAPACITY);
     }
 
     @Override
     public float getPressure(ItemStack itemStack) {
-        return fetchPressure(itemStack);
+        return pressurizableItem.getPressure(itemStack);
     }
 
     @Override
     public void setPressure(ItemStack itemStack, float newStored) {
-        savePressure(itemStack, newStored);
+        pressurizableItem.setPressure(itemStack, newStored);
     }
 
     @Override
     public float getMaxPressure() {
-        return MAX_PRESSURE;
+        return pressurizableItem.getMaxPressure();
     }
 
     @Override
     public FluidStack getFluid(ItemStack itemStack) {
-        return fetchFluidOrCreate(itemStack);
+        return pressurizableItem.getFluid(itemStack);
     }
 
     @Override
     public void setFluid(ItemStack itemStack, FluidStack fluidStack) {
-        saveFluid(itemStack, fluidStack);
+        pressurizableItem.setFluid(itemStack, fluidStack);
     }
 
     @Override
     public float getMaxFluid() {
-        return FLUID_CAPACITY;
+        return pressurizableItem.getMaxFluid();
     }
 
     @Override
@@ -95,88 +96,20 @@ public class ItemHydraulicWrench extends HydraulicItemBase implements IPressuriz
         return true;
     }
 
-    private float fetchPressure(ItemStack container) {
-        if (container.getTagCompound() == null || container.getTagCompound().getTag("pressure") == null) {
-            container.stackTagCompound = new NBTTagCompound();
-            container.stackTagCompound.setFloat("pressure", 0);
-        }
-
-        return container.stackTagCompound.getFloat("pressure");
-    }
-
-    private FluidStack fetchFluidOrCreate(ItemStack container) {
-        if (container.getTagCompound() == null || container.getTagCompound().getTag("fluid") == null) {
-            if (container.stackTagCompound == null)
-                container.stackTagCompound = new NBTTagCompound();
-            container.stackTagCompound.setTag("fluid", new NBTTagCompound());
-        }
-
-        FluidStack existing = FluidStack.loadFluidStackFromNBT((NBTTagCompound) container.stackTagCompound.getTag("fluid"));
-
-        if (existing != null)
-            saveFluid(container, existing);
-
-        return existing;
-    }
-
-    private void saveFluid(ItemStack container, FluidStack newFluid) {
-        if (container.getTagCompound() == null)
-            container.setTagCompound(new NBTTagCompound());
-
-        container.stackTagCompound.setTag("fluid", newFluid.writeToNBT(new NBTTagCompound()));
-    }
-
-    private void savePressure(ItemStack container, float newPressure) {
-        if (container.getTagCompound() == null)
-            container.setTagCompound(new NBTTagCompound());
-
-        container.stackTagCompound.setFloat("pressure", newPressure);
-    }
-
     @Override
     public void addInformation(ItemStack itemStack, EntityPlayer player, List lines, boolean noIdea) {
         super.addInformation(itemStack, player, lines, noIdea);
-        float pressure = fetchPressure(itemStack);
-        FluidStack fluidStack = fetchFluidOrCreate(itemStack);
-        lines.add("Pressure: " + Math.floor(pressure / 1000) + " Bar/" + Math.round(getMaxPressure() / 1000) + " Bar");
-        if (fluidStack != null)
-            lines.add(fluidStack.getFluid().getLocalizedName(fluidStack) + ": " + fluidStack.amount + " mB/" + FLUID_CAPACITY + " mB");
+        pressurizableItem.addInformation(itemStack, lines);
     }
 
     @Override
     public boolean canWrench(EntityPlayer entityPlayer, int i, int i1, int i2) {
-        ItemStack itemStack = entityPlayer.getCurrentEquippedItem();
-        if (itemStack == null || !(itemStack.getItem() instanceof ItemHydraulicWrench))
-            return false;
-
-        ItemHydraulicWrench wrench = (ItemHydraulicWrench) itemStack.getItem();
-        FluidStack fluidStack = fetchFluidOrCreate(itemStack);
-        if (fluidStack == null)
-            return false;
-
-        return wrench.fetchPressure(itemStack) >= PRESSURE_PER_WRENCH && fetchFluidOrCreate(itemStack).amount > 0;
+        return pressurizableItem.canUse(entityPlayer, PRESSURE_PER_WRENCH);
     }
 
     @Override
     public void wrenchUsed(EntityPlayer entityPlayer, int i, int i1, int i2) {
-        ItemStack itemStack = entityPlayer.getCurrentEquippedItem();
-        if (itemStack == null || !(itemStack.getItem() instanceof ItemHydraulicWrench))
-            return;
-
-        ItemHydraulicWrench wrench = (ItemHydraulicWrench) itemStack.getItem();
-        wrench.setPressure(itemStack, wrench.getPressure(itemStack) - PRESSURE_PER_WRENCH);
-        FluidStack fluidStack = wrench.fetchFluidOrCreate(itemStack);
-        if (fluidStack == null || fluidStack.getFluid() != FluidRegistry.WATER)
-            return;
-
-        if (entityPlayer.worldObj.rand.nextFloat() <= CHANCE_TO_RELEASE_WATER) {
-            // oops, there's some leaky pipe!
-            fluidStack.amount -= 1;
-            if (fluidStack.amount == 0)
-                saveFluid(itemStack, null);
-            else
-                saveFluid(itemStack, fluidStack);
-        }
+        pressurizableItem.onItemUse(entityPlayer, CHANCE_TO_RELEASE_WATER, PRESSURE_PER_WRENCH);
     }
 
     @Override
@@ -187,8 +120,8 @@ public class ItemHydraulicWrench extends HydraulicItemBase implements IPressuriz
     public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List par3List) {
         par3List.add(new ItemStack(par1, 1, 0));
         ItemStack filled = new ItemStack(par1, 1);
-        savePressure(filled, getMaxPressure());
-        setFluid(filled, new FluidStack(Fluids.fluidHydraulicOil, (int)getMaxFluid()));
+        pressurizableItem.savePressure(filled, getMaxPressure());
+        setFluid(filled, new FluidStack(Fluids.fluidHydraulicOil, (int) getMaxFluid()));
         par3List.add(filled);
     }
 }
