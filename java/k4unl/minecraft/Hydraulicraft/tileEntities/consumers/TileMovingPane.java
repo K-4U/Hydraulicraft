ad14cf82f7164664a6d6466a018c76887c8bbe69
Koen Beckers <koen@k-4u.nl>
2016-01-25 19:00:54 +0100

    Hate this

--- java/k4unl/minecraft/Hydraulicraft/tileEntities/consumers/TileMovingPane.java
+++ java/k4unl/minecraft/Hydraulicraft/tileEntities/consumers/TileMovingPane.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TileMovingPane extends TileHydraulicBase implements IHydraulicConsumer {
+    //FIXME: I TURN NULL SOMEHOW
     private Location parent;
     private Location child;
     private EnumFacing facing = EnumFacing.UP;
