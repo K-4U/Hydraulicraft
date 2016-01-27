825e73e4f01f10f943400a3760f8ddd7f3413e78
Koen Beckers <koen@k-4u.nl>
2016-01-27 16:29:48 +0100

    Fixed that IGWMod spam.

--- java/k4unl/minecraft/Hydraulicraft/thirdParty/igwmod/IGWSupportNotifier.java
+++ java/k4unl/minecraft/Hydraulicraft/thirdParty/igwmod/IGWSupportNotifier.java
@@ -74,7 +74,7 @@ public class IGWSupportNotifier{
     public void onPlayerJoin(TickEvent.PlayerTickEvent event){
         if(event.player.worldObj.isRemote && event.player == FMLClientHandler.instance().getClientPlayerEntity()) {
             event.player.addChatComponentMessage(IChatComponent.Serializer.jsonToComponent("[\"" + EnumChatFormatting.GOLD + "The mod " + supportingMod + " is supporting In-Game Wiki mod. " + EnumChatFormatting.GOLD + "However, In-Game Wiki isn't installed! " + "[\"," + "{\"text\":\"Download Latest\",\"color\":\"green\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/igwmod_download\"}}," + "\"]\"]"));
-            MinecraftForge.EVENT_BUS.register(this);
+            MinecraftForge.EVENT_BUS.unregister(this);
         }
     }
 
