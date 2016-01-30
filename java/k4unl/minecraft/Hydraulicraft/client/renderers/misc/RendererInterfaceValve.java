7a4d6fca39e8a1e77dddf3908c8fd297e1f68e68
Koen Beckers <koen@k-4u.nl>
2016-01-30 01:07:37 +0100

    Tank grid renders again.

--- java/k4unl/minecraft/Hydraulicraft/client/renderers/misc/RendererInterfaceValve.java
+++ java/k4unl/minecraft/Hydraulicraft/client/renderers/misc/RendererInterfaceValve.java
@@ -1,5 +1,6 @@
 package k4unl.minecraft.Hydraulicraft.client.renderers.misc;
 
+import k4unl.minecraft.Hydraulicraft.client.renderers.IconSupplier;
 import k4unl.minecraft.Hydraulicraft.tileEntities.misc.TileInterfaceValve;
 import k4unl.minecraft.k4lib.client.RenderHelper;
 import k4unl.minecraft.k4lib.lib.Functions;
@@ -77,34 +78,34 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
                     for(int yR = 0; yR < outerYDifference; yR++) {
                         //Draw north side
                         tessellator.setNormal(0, 0, -1);
-                        tessellator.addVertexWithUV(-0.002F + xR, -0.002F + yR, -0.002F, u, v);
-                        tessellator.addVertexWithUV(-0.002F + xR, 0.002F + yR+1, -0.002F, u, V);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, 0.002F + yR+1, -0.002F, U, V);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, -0.002F + yR, -0.002F, U, v);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F + yR, -0.002F, u, v);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, 0.002F + yR+1, -0.002F, u, V);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, 0.002F + yR+1, -0.002F, U, V);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F + yR, -0.002F, U, v);
 
                         //Draw south side
                         tessellator.setNormal(0, 0, 1);
-                        tessellator.addVertexWithUV(-0.002F + xR, -0.002F + yR, outerZDifference+0.002F, u, v);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, -0.002F + yR, outerZDifference+0.002F, u, V);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, 0.002F + yR + 1, outerZDifference+0.002F, U, V);
-                        tessellator.addVertexWithUV(-0.002F + xR, 0.002F + yR + 1, outerZDifference+0.002F, U, v);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F + yR, outerZDifference+0.002F, u, v);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F + yR, outerZDifference+0.002F, u, V);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, 0.002F + yR + 1, outerZDifference+0.002F, U, V);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, 0.002F + yR + 1, outerZDifference+0.002F, U, v);
                     }
                 }
                 for(int zR = 0; zR < outerZDifference; zR++){
                     for(int yR = 0; yR < outerYDifference; yR++) {
                         //Draw west side:
                         tessellator.setNormal(-1, 0, 0);
-                        tessellator.addVertexWithUV(-0.002F, -0.002F + yR, 0.002F + zR + 1, u, v);
-                        tessellator.addVertexWithUV(-0.002F, 0.002F + yR + 1, 0.002F + zR + 1, u, V);
-                        tessellator.addVertexWithUV(-0.002F, 0.002F + yR + 1, -0.002F + zR, U, V);
-                        tessellator.addVertexWithUV(-0.002F, -0.002F + yR, -0.002F + zR, U, v);
+                        RenderHelper.tesselatedTexture(-0.002F, -0.002F + yR, 0.002F + zR + 1, u, v);
+                        RenderHelper.tesselatedTexture(-0.002F, 0.002F + yR + 1, 0.002F + zR + 1, u, V);
+                        RenderHelper.tesselatedTexture(-0.002F, 0.002F + yR + 1, -0.002F + zR, U, V);
+                        RenderHelper.tesselatedTexture(-0.002F, -0.002F + yR, -0.002F + zR, U, v);
 
                         //Draw east side:
                         tessellator.setNormal(1, 0, 0);
-                        tessellator.addVertexWithUV(outerXDifference + 0.002F, -0.002F + yR, -0.002F + zR, u, v);
-                        tessellator.addVertexWithUV(outerXDifference + 0.002F, 0.002F + yR + 1, -0.002F + zR, u, V);
-                        tessellator.addVertexWithUV(outerXDifference + 0.002F, 0.002F + yR + 1, 0.002F + zR + 1, U, V);
-                        tessellator.addVertexWithUV(outerXDifference + 0.002F, -0.002F + yR, 0.002F + zR + 1, U, v);
+                        RenderHelper.tesselatedTexture(outerXDifference + 0.002F, -0.002F + yR, -0.002F + zR, u, v);
+                        RenderHelper.tesselatedTexture(outerXDifference + 0.002F, 0.002F + yR + 1, -0.002F + zR, u, V);
+                        RenderHelper.tesselatedTexture(outerXDifference + 0.002F, 0.002F + yR + 1, 0.002F + zR + 1, U, V);
+                        RenderHelper.tesselatedTexture(outerXDifference + 0.002F, -0.002F + yR, 0.002F + zR + 1, U, v);
                     }
                 }
 
@@ -112,17 +113,17 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
                     for(int xR = 0; xR < outerXDifference; xR++) {
                         //Top side
                         tessellator.setNormal(0, 1, 0);
-                        tessellator.addVertexWithUV(-0.002F + xR, outerYDifference + 0.02F, 0.02F + zR + 1, u, v);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, outerYDifference + 0.02F, 0.02F + zR + 1, u, V);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, outerYDifference + 0.02F, -0.02F + zR, U, V);
-                        tessellator.addVertexWithUV(-0.002F + xR, outerYDifference + 0.02F, -0.02F + zR, U, v);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, outerYDifference + 0.02F, 0.02F + zR + 1, u, v);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, outerYDifference + 0.02F, 0.02F + zR + 1, u, V);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, outerYDifference + 0.02F, -0.02F + zR, U, V);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, outerYDifference + 0.02F, -0.02F + zR, U, v);
 
                         //Bottom side
                         tessellator.setNormal(0, -1, 0);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, -0.002F, 0.002F + zR + 1, u, v);
-                        tessellator.addVertexWithUV(-0.002F + xR, -0.002F, 0.002F + zR + 1, u, V);
-                        tessellator.addVertexWithUV(-0.002F + xR, -0.002F, -0.002F + zR, U, V);
-                        tessellator.addVertexWithUV(0.002F + xR + 1, -0.002F, -0.002F + zR, U, v);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F, 0.002F + zR + 1, u, v);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F, 0.002F + zR + 1, u, V);
+                        RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F, -0.002F + zR, U, V);
+                        RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F, -0.002F + zR, U, v);
                     }
                 }
 
@@ -147,7 +148,8 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
     */
     @Override
     public void renderTileEntityAt(TileEntity tileentity, double x, double y,
-                                   double z, float f, int destroyStage) {
+      double z, float f, int destroyStage) {
+
         TileInterfaceValve t = (TileInterfaceValve) tileentity;
         //Get metadata for rotation:
 
@@ -158,12 +160,12 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
 
         GL11.glPushMatrix();
         GL11.glTranslatef(x, y, z);
-//		FMLClientHandler.instance().getClient().getTextureManager().bindTexture(resLoc);
+        //		FMLClientHandler.instance().getClient().getTextureManager().bindTexture(resLoc);
 
         if (t.isValidTank()) {
             FluidTankInfo tankInfo = t.getTankInfo(EnumFacing.UP)[0];
-            Location corner1 = t.getTankCorner1();
-            Location corner2 = t.getTankCorner2();
+            Location corner1 = new Location(t.getTankCorner1());
+            Location corner2 = new Location(t.getTankCorner2());
 
             int innerXDifference;
             int innerYDifference;
@@ -172,12 +174,85 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
             int locationDifferenceY = t.getPos().getY() - corner1.getY();
             int locationDifferenceZ = t.getPos().getZ() - corner1.getZ();
 
+            int outerXDifference;
+            int outerYDifference;
+            int outerZDifference;
+            corner2.addX(1);
+            corner2.addY(1);
+            corner2.addZ(1);
 
-            innerXDifference = corner2.getX() - corner1.getX() - 1;
-            innerYDifference = corner2.getY() - corner1.getY() - 1;
-            innerZDifference = corner2.getZ() - corner1.getZ() - 1;
+            outerXDifference = corner2.getX() - corner1.getX();
+            outerYDifference = corner2.getY() - corner1.getY();
+            outerZDifference = corner2.getZ() - corner1.getZ();
 
-            GL11.glTranslatef(-locationDifferenceX, -locationDifferenceY, -locationDifferenceZ);
+            //Log.info(corner2_out.printCoords());
+
+            GL11.glEnable(GL11.GL_BLEND);
+
+            GL11.glTranslatef(-(locationDifferenceX), -(locationDifferenceY), -(locationDifferenceZ));
+
+            GL11.glAlphaFunc(GL11.GL_GEQUAL, 0.4F);
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240f, 240f);
+
+            TextureAtlasSprite icon = IconSupplier.tankGrid;
+            float u = icon.getMinU();
+            float v = icon.getMinV();
+            float U = icon.getMaxU();
+            float V = icon.getMaxV();
+
+            boolean wasTessellating = RenderHelper.beginTesselatingWithTexture();
+            for (int xR = 0; xR < outerXDifference; xR++) {
+                for (int yR = 0; yR < outerYDifference; yR++) {
+                    //Draw north side
+
+                    RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F + yR, -0.002F, u, v);
+                    RenderHelper.tesselatedTexture(-0.002F + xR, 0.002F + yR + 1, -0.002F, u, V);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, 0.002F + yR + 1, -0.002F, U, V);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F + yR, -0.002F, U, v);
+
+                    //Draw south side
+                    RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F + yR, outerZDifference + 0.002F, u, v);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F + yR, outerZDifference + 0.002F, u, V);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, 0.002F + yR + 1, outerZDifference + 0.002F, U, V);
+                    RenderHelper.tesselatedTexture(-0.002F + xR, 0.002F + yR + 1, outerZDifference + 0.002F, U, v);
+                }
+            }
+            for (int zR = 0; zR < outerZDifference; zR++) {
+                for (int yR = 0; yR < outerYDifference; yR++) {
+                    //Draw west side:
+                    RenderHelper.tesselatedTexture(-0.002F, -0.002F + yR, 0.002F + zR + 1, u, v);
+                    RenderHelper.tesselatedTexture(-0.002F, 0.002F + yR + 1, 0.002F + zR + 1, u, V);
+                    RenderHelper.tesselatedTexture(-0.002F, 0.002F + yR + 1, -0.002F + zR, U, V);
+                    RenderHelper.tesselatedTexture(-0.002F, -0.002F + yR, -0.002F + zR, U, v);
+
+                    //Draw east side:
+                    RenderHelper.tesselatedTexture(outerXDifference + 0.002F, -0.002F + yR, -0.002F + zR, u, v);
+                    RenderHelper.tesselatedTexture(outerXDifference + 0.002F, 0.002F + yR + 1, -0.002F + zR, u, V);
+                    RenderHelper.tesselatedTexture(outerXDifference + 0.002F, 0.002F + yR + 1, 0.002F + zR + 1, U, V);
+                    RenderHelper.tesselatedTexture(outerXDifference + 0.002F, -0.002F + yR, 0.002F + zR + 1, U, v);
+                }
+            }
+
+            for (int zR = 0; zR < outerZDifference; zR++) {
+                for (int xR = 0; xR < outerXDifference; xR++) {
+                    //Top side
+                    RenderHelper.tesselatedTexture(-0.002F + xR, outerYDifference + 0.02F, 0.02F + zR + 1, u, v);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, outerYDifference + 0.02F, 0.02F + zR + 1, u, V);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, outerYDifference + 0.02F, -0.02F + zR, U, V);
+                    RenderHelper.tesselatedTexture(-0.002F + xR, outerYDifference + 0.02F, -0.02F + zR, U, v);
+
+                    //Bottom side
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F, 0.002F + zR + 1, u, v);
+                    RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F, 0.002F + zR + 1, u, V);
+                    RenderHelper.tesselatedTexture(-0.002F + xR, -0.002F, -0.002F + zR, U, V);
+                    RenderHelper.tesselatedTexture(0.002F + xR + 1, -0.002F, -0.002F + zR, U, v);
+                }
+            }
+
+            
+            //RenderHelper.drawTesselatedCubeWithTexture(new Vector3fMax(-0.02F, -0.02F, -0.02F, outerXDifference + 0.02F, outerYDifference + 0.02F, outerZDifference + 0.02F), IconSupplier.tankGrid);
+            GL11.glDisable(GL11.GL_BLEND);
+
 
             //GL11.glColor3f(1.0F, 1.0F, 1.0F);
             GL11.glEnable(GL11.GL_BLEND);
@@ -186,30 +261,34 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
             GL11.glDisable(GL11.GL_LIGHTING); //Disregard lighting
             //GL11.glBegin(GL11.GL_QUADS);
             OpenGlHelper.setLightmapTextureCoords(OpenGlHelper
-                    .lightmapTexUnit, 240f, 240f);
+              .lightmapTexUnit, 240f, 240f);
 
 
             float percentage;
             if (tankInfo != null) {
                 if (tankInfo.fluid != null) {
+                    innerXDifference = corner2.getX() - corner1.getX() - 1;
+                    innerYDifference = corner2.getY() - corner1.getY() - 1;
+                    innerZDifference = corner2.getZ() - corner1.getZ() - 1;
+
                     percentage = (float) tankInfo.fluid.amount / (float) tankInfo.capacity;
                     int height = (int) Math.floor((double) percentage *
-                            (double) innerYDifference);
+                      (double) innerYDifference);
 
                     TextureAtlasSprite fluidIcon = Functions.getFluidIcon(tankInfo.fluid.getFluid());
 
                     GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
                     //Log.info(height + "");
-                    boolean wasTessellating = RenderHelper.beginTesselatingWithTexture();
+
                     for (int xR = 1; xR <= innerXDifference; xR++) {
                         for (int zR = 1; zR <= innerZDifference; zR++) {
                             if (!tankInfo.fluid.getFluid().isGaseous()) {
                                 boolean bottomRendered = false;
                                 for (int zY = 1; zY <= height; zY++) {
                                     Vector3fMax insides = new Vector3fMax(xR,
-                                            zY, zR,
-                                            xR + 1, zY,
-                                            zR + 1);
+                                      zY, zR,
+                                      xR + 1, zY,
+                                      zR + 1);
                                     insides.setYMax(zY + 0.99999F);
 
                                     if (zY == 1) {
@@ -240,9 +319,9 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
                                 //Log.info(height + "");
                                 if ((percentage * (float) innerYDifference) % 1.0F >= 0.01F) {
                                     Vector3fMax insides = new Vector3fMax(xR,
-                                            height + 1, zR,
-                                            xR + 1, 0,
-                                            zR + 1);
+                                      height + 1, zR,
+                                      xR + 1, 0,
+                                      zR + 1);
                                     insides.setYMax(height + ((percentage * (float) innerYDifference) % 1.0F) + 1);
 
                                     if (!bottomRendered) {
@@ -268,9 +347,9 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
                                 GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.3F + (percentage * 0.7F));
                                 for (int zY = 1; zY <= innerYDifference; zY++) {
                                     Vector3fMax insides = new Vector3fMax(xR + 0.001F,
-                                            zY + 0.001F, zR + 0.001F,
-                                            xR - 0.001F + 1, -0.001F,
-                                            zR - 0.001F + 1);
+                                      zY + 0.001F, zR + 0.001F,
+                                      xR - 0.001F + 1, -0.001F,
+                                      zR - 0.001F + 1);
                                     insides.setYMax(zY - 0.001F + 1);
                                     if (zY == 1) {
                                         RenderHelper.drawTesselatedSideBottomWithTexture(insides, fluidIcon);
@@ -298,15 +377,15 @@ public class RendererInterfaceValve extends TileEntitySpecialRenderer {//impleme
 
                         }
                     }
-                    RenderHelper.stopTesselating(wasTessellating);
-
                 }
             }
+            /*GL11.glTranslatef(locationDifferenceX+1, locationDifferenceY+1, locationDifferenceZ+1);
+            GL11.glTranslatef(-x, -y, -z);*/
+            RenderHelper.stopTesselating(wasTessellating);
             GL11.glDisable(GL11.GL_BLEND);
 
             //RenderHelper.drawColoredCube(insides);
             //GL11.glEnd();
-
 
         }
 
