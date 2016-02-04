9440a5198692a42ddee232d271c5892d7f681dad
Koen Beckers <koen@k-4u.nl>
2016-02-04 16:41:53 +0100

    Would you look at that? i fixed #183

--- java/k4unl/minecraft/Hydraulicraft/blocks/misc/BlockLight.java
+++ java/k4unl/minecraft/Hydraulicraft/blocks/misc/BlockLight.java
@@ -4,97 +4,170 @@ import k4unl.minecraft.Hydraulicraft.blocks.HCBlocks;
 import k4unl.minecraft.Hydraulicraft.blocks.HydraulicBlockBase;
 import k4unl.minecraft.Hydraulicraft.items.HCItems;
 import k4unl.minecraft.Hydraulicraft.items.ItemMiningHelmet;
+import k4unl.minecraft.Hydraulicraft.lib.Properties;
 import k4unl.minecraft.Hydraulicraft.lib.config.Names;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Random;
 
 public class BlockLight extends HydraulicBlockBase {
-	public static final int maxLightLevel = (int)(0.9F * 15);
-	
-	
-	public BlockLight(){
-		super(Names.blockLight, Material.rock, false);
-		setLightOpacity(15);
-		setTickRandomly(false);
-	}
-	
-	@Override
-	public boolean getTickRandomly(){
-		return true;
-	}
-	
-	public int getRenderType(){
+
+    public static final int maxLightLevel = (int) (0.9F * 15);
+
+
+    public BlockLight() {
+
+        super(Names.blockLight, Material.air, false);
+        setLightOpacity(15);
+        setTickRandomly(false);
+        setDefaultState(this.blockState.getBaseState().withProperty(Properties.LIGHTVALUE, 15));
+        setBlockBounds(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+    }
+
+    @Override
+    public boolean getTickRandomly() {
+
+        return true;
+    }
+
+    public int getRenderType() {
+
         return -1;
     }
 
-	@Override
-	public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state) {
-		return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-	}
+    @Override
+    public void setBlockBoundsBasedOnState(IBlockAccess worldIn, BlockPos pos) {
 
-	@Override
-	public AxisAlignedBB getSelectedBoundingBox(World worldIn, BlockPos pos) {
-		return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-	}
+        this.setBlockBounds(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+    }
 
-	/**
-     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
-     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
-     */
-    public boolean isOpaqueCube()
-    {
+    @Override
+    public void addCollisionBoxesToList(World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List<AxisAlignedBB> list, Entity collidingEntity) {
+
+        super.addCollisionBoxesToList(worldIn, pos, state, mask, list, collidingEntity);
+    }
+
+    @Override
+    public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCollidable() {
+
         return false;
     }
 
-	@Override
-	public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid) {
-		return false;
-	}
 
-	@Override
-	public void dropBlockAsItemWithChance(World worldIn, BlockPos pos, IBlockState state, float chance, int fortune) {
+    @Override
+    public boolean isFullBlock() {
 
-	}
-
-	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-    	//if(world.getBlock(x, y, z) instanceof BlockLight){
-	    	//Seach within 3 blocks for a player.
-	    	//If no player found. Remove the block
-    		EntityPlayer closestPlayer = worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 15);
-	    	if(closestPlayer == null){
-	    		worldIn.setBlockToAir(pos);
-	    	}else{
-	    		if(closestPlayer.getCurrentArmor(3) != null){
-		    		if(closestPlayer.getCurrentArmor(3).getItem() == HCItems.itemMiningHelmet){
-		    			if(!ItemMiningHelmet.isPoweredOn(closestPlayer.getCurrentArmor(3))){
-		    				worldIn.setBlockToAir(pos);
-		    			}
-		    		}else{
-		    			worldIn.setBlockToAir(pos);
-		    		}
-	    		}else{
-	    			worldIn.setBlockToAir(pos);
-	    		}
-	    	}
-    	//}
-	    //world.scheduleBlockUpdate(x, y, z, HCBlocks.blockLight, 10);
+        return false;
     }
 
-	@Override
-	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-    	worldIn.scheduleBlockUpdate(pos, HCBlocks.blockLight, 10, 10);
+    @Override
+    public AxisAlignedBB getSelectedBoundingBox(World worldIn, BlockPos pos) {
+
+        return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state) {
+
+        return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+    }
+
+    /**
+     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
+     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
+     */
+    public boolean isOpaqueCube() {
+
+        return false;
+    }
+
+    @Override
+    public boolean canCollideCheck(IBlockState state, boolean hitIfLiquid) {
+
+        return false;
+    }
+
+    @Override
+    public void dropBlockAsItemWithChance(World worldIn, BlockPos pos, IBlockState state, float chance, int fortune) {
+
+    }
+
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        //if(world.getBlock(x, y, z) instanceof BlockLight){
+        //Seach within 3 blocks for a player.
+        //If no player found. Remove the block
+        EntityPlayer closestPlayer = worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 15);
+        if (closestPlayer == null) {
+            worldIn.setBlockToAir(pos);
+        } else {
+            if (closestPlayer.getCurrentArmor(3) != null) {
+                if (closestPlayer.getCurrentArmor(3).getItem() == HCItems.itemMiningHelmet) {
+                    if (!ItemMiningHelmet.isPoweredOn(closestPlayer.getCurrentArmor(3))) {
+                        worldIn.setBlockToAir(pos);
+                    }
+                } else {
+                    worldIn.setBlockToAir(pos);
+                }
+            } else {
+                worldIn.setBlockToAir(pos);
+            }
+        }
+        //}
+        //world.scheduleBlockUpdate(x, y, z, HCBlocks.blockLight, 10);
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+
+        worldIn.scheduleBlockUpdate(pos, HCBlocks.blockLight, 10, 10);
     }
 
 
-	@Override
-	public int getLightValue(IBlockAccess world, BlockPos pos) {
-		return 15;//world.getBlockMetadata(x, y, z);
+    @Override
+    public int getLightValue(IBlockAccess world, BlockPos pos) {
+
+        return getMetaFromState(world.getBlockState(pos));
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+
+        return getDefaultState().withProperty(Properties.LIGHTVALUE, 15);
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+
+        return new BlockState(this, Properties.LIGHTVALUE);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+
+        return getDefaultState().withProperty(Properties.LIGHTVALUE, meta >> 2);
+
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+
+        return state.getValue(Properties.LIGHTVALUE);
     }
 }
--- java/k4unl/minecraft/Hydraulicraft/lib/Properties.java
+++ java/k4unl/minecraft/Hydraulicraft/lib/Properties.java
@@ -3,6 +3,7 @@ package k4unl.minecraft.Hydraulicraft.lib;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.util.EnumFacing;
 
 /**
@@ -23,6 +24,8 @@ public final class Properties {
     public static final PropertyEnum<EnumFacing> FACING = PropertyEnum.create("facing", EnumFacing.class);
 
     public static final PropertyBool CHILD = PropertyBool.create("child");
+
+    public static final PropertyInteger LIGHTVALUE = PropertyInteger.create("lightValue", 0, 15);
 
     private Properties() {
 
